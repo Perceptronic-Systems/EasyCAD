665813d4ac7d2520906412b2a78ce538906bba4a
Perceptronic-Systems <andrewkirk006@gmail.com>
2026-05-11 08:11:12 -0600

    Fixed keybinds blocking other keys

--- scripts/keybinds.js
+++ scripts/keybinds.js
@@ -2,12 +2,13 @@ import { selectAll, copy, paste, deselectObjects, booleanToSelection, removeObje
 import { unselectTool, setTool } from "./editor_controls.js";
 
 document.addEventListener('keydown', (event) => {
-  event.preventDefault();
   switch (event.key.toLowerCase()) {
     case "escape":
+      event.preventDefault();
       unselectTool()
       break;
     case 'delete':
+      event.preventDefault();
       if (confirm("Are you sure you would like to delete selected objects?")) {
         for (const name of Object.keys(selectedObjects)) {
           removeObject(name);
@@ -15,6 +16,7 @@ document.addEventListener('keydown', (event) => {
         deselectObjects();
       }
     case 'a':
+      event.preventDefault();
       if (ctrlDown) {
         if (!shiftDown) {
           selectAll();
@@ -24,34 +26,43 @@ document.addEventListener('keydown', (event) => {
       }
       break;
     case 'g':
+      event.preventDefault();
       setTool('move');
       break;
     case 'r':
+      event.preventDefault();
       setTool('rotate');
       break;
     case 's':
+      event.preventDefault();
       setTool('scale');
       break;
     case 'c':
+      event.preventDefault();
       if (ctrlDown) {
         copy();
       }
       break;
     case 'm':
+      event.preventDefault();
       if (ctrlDown) booleanToSelection('merge', 'Combined Part');
       break;
     case 'o':
+      event.preventDefault();
       if (ctrlDown) booleanToSelection('subtract', 'Combined Part');
       break;
     case 'i':
+      event.preventDefault();
       if (ctrlDown) booleanToSelection('intersect', 'Combined Part');
       break;
     case 'v':
+      event.preventDefault();
       if (ctrlDown) {
         paste();
       }
       break;
     case 'd':
+      event.preventDefault();
       if (ctrlDown) {
         copy();
         paste();
